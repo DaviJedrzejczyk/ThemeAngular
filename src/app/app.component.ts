c475f032d98b8f2748b53f1da7f5df9c383579f2
SprilerBr <Djedrzejczyk88@gmail.com>
2024-04-01 16:21:18 -0300

    Theme Angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from './core/components/header/header.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  imports: [RouterOutlet, HeaderComponent],
+  template: `<app-header />
+    <router-outlet /> `,
 })
-export class AppComponent {
-  title = 'curso-angular-tema-dark';
-}
+export class AppComponent {}
